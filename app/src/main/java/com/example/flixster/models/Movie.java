304e1c77096cc2ba41524ba79049d14cae9b8840
crawlive <crawfolivia@gmail.com>
2021-09-18 23:18:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,39 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {      //whoever calls this method is responsible for handling exception
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("original_title");
+        overview = jsonObject.getString("overview");
+    }
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++){       //adding movie at e@ pos in arr
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() { //to do this properly: 20:28 Android U Week 1
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
